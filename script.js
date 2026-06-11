138ebaa289ec744c1cd015981ffb138936a055a7
carvalho-dev <129604515+carvalho-dev@users.noreply.github.com>
2026-06-10 23:16:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 const form = document.getElementById("loginForm");
 
+window.addEventListener("load", () => {
+    const savedEmail = localStorage.getItem("savedEmail");
+
+    if (savedEmail) {
+        document.getElementById("email").value = savedEmail;
+        document.getElementById("remember").checked = true;
+    }
+});
+
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+    const remember = document.getElementById("remember").checked;
 
-    // Usuário e senha de exemplo
     const validEmail = "admin@email.com";
     const validPassword = "123456";
 
     if (email === validEmail && password === validPassword) {
-        alert("Login realizado com sucesso!");
 
-        // Redirecionar para outra página
+        if (remember) {
+            localStorage.setItem("savedEmail", email);
+        } else {
+            localStorage.removeItem("savedEmail");
+        }
+
+        alert("Login realizado com sucesso!");
         window.location.href = "home.html";
+
     } else {
         alert("E-mail ou senha incorretos!");
     }
